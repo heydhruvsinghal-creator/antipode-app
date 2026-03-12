df5aa36b2f77245d0f9834b3b93ec8ca0e850daf
heydhruvsinghal-creator <heydhruvsinghal@gmail.com>
2026-03-13 03:32:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -55,4 +55,5 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT
+    console.log(`Server running on port ${PORT}`);
+});
