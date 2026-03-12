2b13ede561883eb6531386cc47988398e41c06ec
heydhruvsinghal-creator <heydhruvsinghal@gmail.com>
2026-03-13 02:57:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,17 +9,23 @@ const io = new Server(server);
 
 app.use(express.static(path.join(__dirname, '.')));
 
+// Shared room for friends
+const SHARED_ROOM = 'antipode-friends';
+
 io.on('connection', (socket) => {
     console.log('A user connected');
 
     socket.on('join_room', (room) => {
         socket.join(room);
         console.log(`User joined room: ${room}`);
-        io.to(room).emit('chat_message', 'Someone from the other side of the world has joined.');
+        
+        // Send a system message to the room
+        io.to(room).emit('chat_message', 'Someone from the other side of the world has joined!');
     });
 
     socket.on('chat_message', (msg) => {
-        io.emit('chat_message', msg);
+        // Broadcast message to everyone in the room
+        io.to(SHARED_ROOM).emit('chat_message', msg);
     });
 
     socket.on('disconnect', () => {
