b6e4f07ad68122dd804f76a61c1524eedcbf66f2
heydhruvsinghal-creator <heydhruvsinghal@gmail.com>
2026-03-13 03:14:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,21 +9,16 @@ const io = new Server(server);
 
 app.use(express.static(path.join(__dirname, '.')));
 
-// Shared room for friends
 const SHARED_ROOM = 'antipode-friends';
-
-// Track connected users with their names
 const users = new Map();
 
 io.on('connection', (socket) => {
     console.log('A user connected');
 
-    // Generate a random temporary username
     const tempUsername = `User_${Math.floor(Math.random() * 1000)}`;
     users.set(socket.id, { id: socket.id, name: tempUsername });
 
     socket.on('set_username', (username) => {
-        // Update user's name
         const user = users.get(socket.id);
         if (user) {
             user.name = username;
@@ -35,10 +30,8 @@ io.on('connection', (socket) => {
         socket.join(room);
         console.log(`User joined room: ${room}`);
         
-        // Send user list to all users
         io.to(room).emit('user_list', Array.from(users.values()).map(u => u.name));
         
-        // Notify others
         const user = users.get(socket.id);
         if (user) {
             socket.to(room).emit('user_joined', user.name);
@@ -46,7 +39,6 @@ io.on('connection', (socket) => {
     });
 
     socket.on('chat_message', (msg) => {
-        // Broadcast message to everyone in the room
         io.to(SHARED_ROOM).emit('chat_message', msg);
     });
 
@@ -63,5 +55,4 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+    console.log(`Server running on port ${PORT
